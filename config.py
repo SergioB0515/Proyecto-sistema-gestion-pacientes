facc475e07b614104b0dc788e35be87e9c2d3b18
SergioB0515 <sergiofonseca968@gmail.com>
2026-09-16 23:34:34 -0500

    Update MYSQL_PASSWORD in development_config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class development_config():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1025531751Sf.'
+    MYSQL_PASSWORD = '108456654654'
     MYSQL_DB = 'proyecto'
     
 config= {
